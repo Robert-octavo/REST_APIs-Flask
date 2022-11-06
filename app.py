1564e6033c2d1f1b2d631662535cb20d33e9b2f1
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-06 13:58:33 -0500

    Fix some errors on app.config

--- app.py
+++ app.py
@@ -16,12 +16,14 @@ def create_app(db_url=None):
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     # smorest will use this for documentation
-    app.config["OPENAPI_VERSION"] = "4.15.2"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
-        "DATABASE+URL", "sqlite:///data.db")
+        "DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
