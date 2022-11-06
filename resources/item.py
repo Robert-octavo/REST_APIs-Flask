d24ddf545bef0d09e769cffdee90b4e42fb9e8c2
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-06 13:55:46 -0500

    Change item and store from resources

--- resources/item.py
+++ resources/item.py
@@ -2,8 +2,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import items, stores
+from db import db
 from schemas import ItemSchema, ItemUpdateSchema
+from models import ItemModel
+from sqlalchemy.exc import SQLAlchemyError
 
 
 blp = Blueprint("items", __name__, description="Operations on items")
@@ -13,42 +15,73 @@ blp = Blueprint("items", __name__, description="Operations on items")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        return item
+        """
         try:
             return items[item_id]
         except KeyError:
             abort(404, message="Store not found")
+        """
 
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        # item_data = request.get_json()
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
 
+        db.session.add(item)
+        db.session.commit()
+        return item
+        # item_data = request.get_json()
+        """
         try:
             item = items[item_id]
             item |= item_data  # Update the item dictionay with the info in the item_data
         except KeyError:
             abort(404, message="Item not found.")
+        """
 
     def delete(self, item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        db.session.delete(item)
+        db.session.commit()
+        return {"message": "Item deleted"}, 200
+
+        #raise NotImplementedError("Deleting an item is not implemented.")
+        """
         try:
             del items[item_id]
             return {"message": "Item deleted."}
         except KeyError:
             abort(404, message="Item not found")
+        """
 
 
 @blp.route("/item")
 class ItemList(MethodView):
-    @blp.response(00, ItemSchema(many=True))
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return items.values()
+        return ItemModel.query.all()
         # return {"items": list(items.values())}
 
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
-        # item_data = request.get_json() it's send from the schema
+        item = ItemModel(**item_data)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error ocurred while inserting the item.")
 
+        return item
+        # item_data = request.get_json() it's send from the schema
+        """
         # check if the item already exists
         for item in items.values():
             if (
@@ -66,3 +99,4 @@ class ItemList(MethodView):
         item = {**item_data, "id": item_id}
         items[item_id] = item
         return item, 201
+        """
--- resources/store.py
+++ resources/store.py
@@ -2,8 +2,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+from db import db
 from schemas import StoreSchema
+from models import StoreModel
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 
 blp = Blueprint("stores", __name__, description="Operations on Stores")
@@ -13,28 +15,50 @@ blp = Blueprint("stores", __name__, description="Operations on Stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
+        store = StoreModel.get_or_404(store_id)
+        return store
+        """
         try:
             return stores[store_id]
         except KeyError:
             abort(404, message="Store not found")
+        """
 
     def delete(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}, 200
+        # raise NotImplementedError("Deleting a store is not implemented.")
+        """
         try:
             del stores[store_id]
             return {"message": "Store deleted."}
         except KeyError:
             abort(404, message="Store not found")
+        """
 
 
 @blp.route("/store")
 class StoreList(MethodView):
     @blp.response(200, StoreSchema)
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
 
     @blp.arguments(StoreSchema)
-    @blp.response(200, StoreSchema)
+    @blp.response(201, StoreSchema)
     def post(self, store_data):
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(400, message="A store with that name already exists.")
+        except SQLAlchemyError:
+            abort(500, message="An error ocurred creating the store.")
+
+        return store
+        """
         # store_data = request.get_json()
         # check if the store exists
         for store in stores.values():
@@ -46,3 +70,4 @@ class StoreList(MethodView):
         store = {**store_data, "id": store_id}
         stores[store_id] = store
         return store, 201
+        """
