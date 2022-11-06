c7c802e814e8ff016bf5fe9a8d7970619425d610
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-06 13:56:25 -0500

    Fix dataType in the squemas

--- schemas.py
+++ schemas.py
@@ -2,24 +2,25 @@ from marshmallow import Schema, fields
 
 
 class PlainItemSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
 
 
 class PlainStoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
+    #store_id = fields.Int()
 
 
 class ItemSchema(PlainItemSchema):
-    store_id = fields.Str(required=True, load_only=True)
-    store = fields.Nested(PlainItemSchema(), dump_only=True)
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
 
 class StoreSchema(PlainStoreSchema):
