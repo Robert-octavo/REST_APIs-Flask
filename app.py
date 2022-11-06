f289950113a1dd5168e71a5f976f339075638c24
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-05 21:41:24 -0500

    Update app to use SqlAlchemy, Create a SQLite db

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
+import os
 from flask import Flask
 from flask_smorest import Api
+from db import db
+import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
 
-app = Flask(__name__)
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-# Config Options
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-# smorest will use this for documentation
-app.config["OPENAPI_VERSION"] = "4.15.2"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    # Config Options
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    # smorest will use this for documentation
+    app.config["OPENAPI_VERSION"] = "4.15.2"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE+URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
-api = Api(app)
+    api = Api(app)
 
+    """
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+    """
 
-api.register_blueprint(ItemBlueprint)
-api.register_blueprint(StoreBlueprint)
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+
+    return app
