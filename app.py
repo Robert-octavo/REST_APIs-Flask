35734185ee253cc746321d923b45c384a571d57d
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-08 15:34:20 -0500

    Add custom messages to jwt errors

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from db import db
@@ -35,6 +35,27 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "robert"
     jwt = JWTManager(app)
 
+    # Update message from JWT
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.",
+                    "error": "token_expired"}), 401
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify({"message": "Signature verification failed.",
+                    "error": "Invalid_token"}), 401
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify({"description": "Request does not contain an access token.",
+                    "error": "authorization_required"}), 401
+        )
     """
     @app.before_first_request
     def create_tables():
